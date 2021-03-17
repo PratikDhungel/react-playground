c88b1f63cca65129916b96ca194388b5b3dc7c6a
Pratik Dhungel <dhungel.1996@gmail.com>
2021-03-17 09:12:31 +0545

    Update navbar styling

--- src/Containers/CardsContainer.tsx
+++ src/Containers/CardsContainer.tsx
@@ -5,19 +5,19 @@ const CardsContainer = () => {
   return (
     <Container fluid style={{ marginTop: '2em' }}>
       <Row noGutters>
-        <Col className='columns' sm={4}>
+        <Col className='columns' md={4}>
           <Card>
             This is card content This is card content This is card content This is card content This is card content This is
             card content This is card content This is card content
           </Card>
         </Col>
-        <Col className='columns' sm={4}>
+        <Col className='columns' md={4}>
           <Card>
             This is card content This is card content This is card content This is card content This is card content This is
             card content This is card content This is card content
           </Card>
         </Col>
-        <Col className='columns' sm={4}>
+        <Col className='columns' md={4}>
           <Card>
             This is card content This is card content This is card content This is card content This is card content This is
             card content This is card content This is card content
--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -9,24 +9,20 @@ const NavBar = () => {
 
   return (
     <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href='#home'>Rent Out</Navbar.Brand>
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='ml-auto'>
-          <Nav.Link href='#features' className='nav-links-spacing'>
-            Features
+          <Nav.Link href='' className='nav-links-spacing nav-links-styling'>
+            Home
           </Nav.Link>
-          <Nav.Link href='#pricing' className='nav-links-spacing'>
-            Pricing
+          <Nav.Link href='#pricing' className='nav-links-spacing nav-links-styling'>
+            My Rentals
           </Nav.Link>
-          <Nav.Link href='#deets' className='nav-links-spacing'>
-            More deets
-          </Nav.Link>
-          <Nav.Link href='#memes' className='nav-links-spacing'>
-            Dank memes
+          <Nav.Link href='#deets' className='nav-links-spacing nav-links-styling'>
+            Account
           </Nav.Link>
         </Nav>
-        {/* </Nav> */}
       </Navbar.Collapse>
     </Navbar>
   );
