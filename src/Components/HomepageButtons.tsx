efa92eb57c8c228a92127fdf2ce5b4cad31e958d
Pratik Dhungel <dhungel.1996@gmail.com>
2021-03-29 15:51:20 +0545

    Add Modal on button click

--- src/Components/HomepageButtons.tsx
+++ src/Components/HomepageButtons.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const HomepageButtons = () => {
+  const [showNewRentalModal, setShowNewRentalModal] = useState<boolean>(false);
+
   return (
-    <div className='homepage-buttons-container'>
-      <Button variant='primary'>Add New Rental</Button>
-    </div>
+    <>
+      <div className='homepage-buttons-container'>
+        <Button variant='primary' onClick={() => setShowNewRentalModal(!showNewRentalModal)}>
+          Add New Rental
+        </Button>
+      </div>
+      <Modal
+        show={showNewRentalModal}
+        keyboard={true}
+        onHide={() => setShowNewRentalModal(!showNewRentalModal)}
+        centered
+        // dialogClassName='homepage-filters-modal'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Rental</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <button className='btn btn-filter-modal__save-btn'>Add</button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
