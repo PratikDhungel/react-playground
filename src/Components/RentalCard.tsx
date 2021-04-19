5010492598b8ec16086271fdcf2f1ac761e4953b
Pratik Dhungel <dhungel.1996@gmail.com>
2021-04-19 17:55:33 +0545

    Variable name update

--- src/Components/RentalCard.tsx
+++ src/Components/RentalCard.tsx
@@ -13,15 +13,15 @@ import { Col, Card } from 'react-bootstrap';
 const RentalCard = ({ ...props }) => {
   const { manufacturer, modelName, distance, thumbnails } = props;
   console.log(thumbnails);
-  const [currentImage, setCurrentImage] = useState<number>(0);
+  const [currentImagePosition, setcurrentImagePosition] = useState<number>(0);
   const lengthOfSlides: number = thumbnails.length;
 
   const nextSlide = (): void => {
-    setCurrentImage(currentImage === lengthOfSlides - 1 ? 0 : currentImage + 1);
+    setcurrentImagePosition(currentImagePosition === lengthOfSlides - 1 ? 0 : currentImagePosition + 1);
   };
 
   const prevSlide = (): void => {
-    setCurrentImage(currentImage === 0 ? lengthOfSlides - 1 : currentImage - 1);
+    setcurrentImagePosition(currentImagePosition === 0 ? lengthOfSlides - 1 : currentImagePosition - 1);
   };
 
   return (
@@ -30,8 +30,8 @@ const RentalCard = ({ ...props }) => {
         <FaChevronLeft className='arrow-btn arrow-btn__left' onClick={() => prevSlide()} />
         {thumbnails.map((imageURL: any, index: number) => {
           return (
-            <div className={index === currentImage ? 'slide-active' : 'slide'} key={index}>
-              {index === currentImage && <img src={imageURL} alt='IMAGE' />}
+            <div className={index === currentImagePosition ? 'slide-active' : 'slide'} key={index}>
+              {index === currentImagePosition && <img src={imageURL} alt='IMAGE' />}
             </div>
           );
         })}
